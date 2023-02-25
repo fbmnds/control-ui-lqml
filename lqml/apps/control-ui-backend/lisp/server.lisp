69d80f1b17914733c7c9d44fe7188cd67d66ece2
fbmnds7 <fbmnds7@fn.de>
2023-02-25 07:40:53 +0100

    Remove duplicated utility functions

--- lqml/apps/control-ui-backend/lisp/server.lisp
+++ lqml/apps/control-ui-backend/lisp/server.lisp
@@ -1,6 +1,6 @@
 (in-package :app)
 
-(defun substring (start end s)
+(defun substring (s start &optional end)
   "Return the substring of `s' from `start' to `end'.
 It uses `subseq' with differences:
 - argument order, s at the end
@@ -25,25 +25,8 @@ It uses `subseq' with differences:
               ""
               (subseq s start end))))))
 
-(defun starts-with-p (start s &key ignore-case)
-  "Return t if S starts with the substring `START', nil otherwise.
-  START can be a string or a character."
-  (let ((start-length (length (string start))))
-    (when (>= (length s) start-length)
-      (let ((fn (if ignore-case #'string-equal #'string=)))
-        (funcall fn s start :start1 0 :end1 start-length)))))
-
-(defun ends-with-p (end s &key ignore-case)
-  "Return t if s ends with the substring 'end', nil otherwise.
-  END can be a character or a string."
-  (let ((s-length (length s))
-        (end-length (length (string end))))
-    (when (>= s-length end-length)
-      (let ((fn (if ignore-case #'string-equal #'string=)))
-        (funcall fn s end :start1 (- s-length end-length))))))
-
 (defun websocket-server-connect (src socket)
-  (qlog "websocket-server-connect " (substring 0 20 src)
+  (qlog "websocket-server-connect " (substring src 0 20)
         " " (q< |objectName| *caller*)
         " " (q< |objectName| ui:*server*)
         " " (format nil "~a" (q< |url| socket))
@@ -51,20 +34,20 @@ It uses `subseq' with differences:
   (let ((server ui:*server*)
         (ws-url (format nil "~a" (q< |url| socket))))
     (q! |appendMessage| ui:*wrect*
-        (str+ "for url: " ws-url " " (substring 0 20 src)))
-    (cond ((ends-with-p "/werkstattlicht/" ws-url) ; '?' omitted in socket.url
+        (str+ "for url: " ws-url " " (substring src 0 20)))
+    (cond ((x:ends-with "/werkstattlicht/" ws-url) ; '?' omitted in socket.url
            (let ((wsl-status (q< |wslStatus| ui:*rect3*)))
              (qlog (str+ "return on /werkstattlicht/? " wsl-status))
              (q! |sendTextMessage| server wsl-status)))
-          ((ends-with-p "/werkstattlicht/r1" ws-url)
+          ((x:ends-with "/werkstattlicht/r1" ws-url)
            (werkstattlicht "/r1"))
-          ((starts-with-p "<?xml" src)
+          ((x:starts-with "<?xml" src)
            (q> |source| ui:*svg* (str+ "data:image/svg+xml;utf8," src)))
-          ((starts-with-p "data:image/svg+xml;utf8," src)
+          ((x:starts-with "data:image/svg+xml;utf8," src)
            (q> |source| ui:*svg* src))
-          ((starts-with-p "[" src)
+          ((x:starts-with "[" src)
            (put-svg src)
            (q! |setMessage| ui:*wrect* (q< |svgMsg| ui:*rect3*)))
-          (t (q! |setMessage| ui:*wrect* (str+ "ignoring " (substring 0 20 src)
+          (t (q! |setMessage| ui:*wrect* (str+ "ignoring " (substring src 0 20)
                                                " for url " ws-url)))))
   (values))
